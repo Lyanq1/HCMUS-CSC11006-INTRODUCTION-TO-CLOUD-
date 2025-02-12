bb8cf2640ee329b2e443da234f50dd9b7752ff53
Lyan <22127344@student.hcmus.edu.vn>
2025-02-13 02:01:46 +0700

    a

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('Welcome to the server');
+	res.send('Welcome to the server at port 5000');
 });
 app.listen(process.env.PORT || 5000, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
