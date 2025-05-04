c9e7dda3c477324001fe6f711021f33e4a63cddb
lyanq <anhquan2004162@gmail.com>
2025-05-05 03:33:50 +0700

    finall

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,19 +11,31 @@ app.use(express.json());
 
 app.use('/api/task', TaskListRoutes);
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+const url = "mongodb://localhost:27017/tasklist";
+
+mongoose.connect(url, {}).then(() => {
 	console.log('Connected to the database');
 }
 ).catch(err => {
 	console.log('Failed to connect to the database');
 	console.log(err);
-}
-);
+});
+
+
+
+// mongoose.connect(process.env.MONGO_URI).then(() => {
+// 	console.log('Connected to the database');
+// }
+// ).catch(err => {
+// 	console.log('Failed to connect to the database');
+// 	console.log(err);
+// }
+// );
 
 app.get('/', (req, res) => {
 	res.send('Welcome to the server at port 5000');
 });
 
-app.listen(process.env.PORT || 80, () => {
-	console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(  5000, () => {
+	console.log(`Server is running on port ${5000}`);
 });
